9110b352b9d8fc7e9545b96b757df67cd59bf374
Farid Rener <proteusvacuum@gmail.com>
2015-02-22 19:46:46 -0500

    use full input

--- jugglefest/juggle_fest.rb
+++ jugglefest/juggle_fest.rb
@@ -3,7 +3,7 @@ Dir[File.dirname(__FILE__) + '/models/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/controllers/*.rb'].each {|file| require file }
 
 jfm = JuggleFestMatcher.new
-jfm.parse_input("data")
+jfm.parse_input("data1")
 jfm.add_jugglers_to_circuits
 jfm.make_offers
 puts jfm.circuits[1970].offers.inject{|sum, i| sum + i }
